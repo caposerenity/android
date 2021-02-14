3f28105391eec68fa84f3cc496013f162f3b6875
lyxxn0414 <181250077@smail.nju.edu.cn>
2021-02-14 14:07:37 +0800

    fix

--- my/app/src/main/java/com/example/chapter3/demo/manager/ManagerFragment.java
+++ my/app/src/main/java/com/example/chapter3/demo/manager/ManagerFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.chapter3.demo.R;
 
@@ -20,12 +24,30 @@ public class ManagerFragment extends Fragment {
     }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manager, container, false);
-    }
+        View view=inflater.inflate(R.layout.fragment_manager, container, false);
+        Spinner spinner=view.findViewById(R.id.mspinner);
+        //后续在此编辑选择条件
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        Toast.makeText(getActivity(),"全部",Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getActivity(),"未提交",Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getActivity(),"已提交",Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
 
-    @Override
-    public void onCreate(Bundle savedInstance) {
-        super.onCreate(savedInstance);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+        return view;
     }
 }
